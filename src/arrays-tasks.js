002f18a4a75f3e2777fca1bb7926e4b94aefcf00
ilyha1003 <ilyhasankovich@gmail.com>
2024-12-19 16:50:02 +0300

    feat: solve some tasks of array-tasks.js

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -1,247 +1,65 @@
-/* ********************************************************************************************
- *                                                                                            *
- * Please read the following tutorial before implementing tasks:                               *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array     *
- *                                                                                            *
- * NOTE : Please do not use loops! All tasks can be implemented using standard Array methods  *
- *                                                                                            *
- ******************************************************************************************** */
-
-/**
- * Creates an array of integers from the specified start to end (inclusive).
- *
- * @param {number} start - The first number of an array.
- * @param {number} end - The last number of an array.
- * @return {array} - An array of integers.
- *
- * @example
- *    getIntervalArray(1, 5)  => [ 1, 2, 3, 4, 5 ]
- *    getIntervalArray(-2, 2)  => [ -2, -1, 0, 1, 2 ]
- *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
- *    getIntervalArray(3, 3) => [ 3 ]
- */
 function getIntervalArray(start, end) {
   return Array.from({ length: end - start + 1 }, (_, index) => start + index);
 }
 
-/**
- * Returns a new array where each element is the sum of the corresponding elements
- * from two arrays. Arrays can have different lengths.
- *
- * @param {array} arr1 - The first array.
- * @param {array} arr2 - The second array.
- * @return {array} - An array containing the sum of corresponding elements.
- *
- * @example
- *    sumArrays([1, 2, 3], [4, 5, 6]) => [5, 7, 9]
- *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
- *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
- */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  return arr1.length > arr2.length
+    ? arr1.map((el, index) => el + (arr2[index] || 0))
+    : arr2.map((el, index) => el + (arr1[index] || 0));
 }
 
-/**
- * Returns an index of the specified element in array or -1 if element is not found.
- *
- * @param {array} arr - The input array.
- * @param {any} value - Element to search.
- * @return {number} - An index of the specified element.
- *
- * @example
- *    findElement(['Ace', 10, true], 10) => 1
- *    findElement(['Array', 'Number', 'string'], 'Date') => -1
- *    findElement([0, 1, 2, 3, 4, 5], 5) => 5
- */
 function findElement(arr, value) {
   return arr.indexOf(value);
 }
 
-/**
- * Returns a number of all occurrences of the specified item in an array.
- *
- * @param {array} arr - The input array.
- * @param {any} item - Element to search.
- * @return {number} - Number of found items.
- *
- * @example
- *    findAllOccurrences([ 0, 0, 1, 1, 1, 2 ], 1) => 3
- *    findAllOccurrences([ 1, 2, 3, 4, 5 ], 0) => 0
- *    findAllOccurrences([ 'a','b','c','c' ], 'c') => 2
- *    findAllOccurrences([ null, undefined, null ], null) => 2
- *    findAllOccurrences([ true, 0, 1, 'true' ], true) => 1
- */
 function findAllOccurrences(arr, item) {
   return arr.filter((el) => el === item).length;
 }
 
-/**
- * Removes falsy values from the specified array.
- * Falsy values: false, null, 0, "", undefined, and NaN.
- *
- * @param {array} arr - The input array.
- * @return {array} - The array without falsy values.
- *
- * @example
- *    removeFalsyValues([ 0, false, 'cat', NaN, true, '' ]) => [ 'cat', true ]
- *    removeFalsyValues([ 1, 2, 3, 4, 5, 'false' ]) => [ 1, 2, 3, 4, 5, 'false' ]
- *    removeFalsyValues([ false, 0, NaN, '', undefined ]) => [ ]
- */
 function removeFalsyValues(arr) {
   return arr.filter((el) => Boolean(el) === true);
 }
 
-/**
- * Returns an array containing the lengths of each string in a specified array of strings.
- *
- * @param {array} arr - The input array.
- * @return {array} - The array of string lengths.
- *
- * @example
- *    getStringsLength([ '', 'a', 'bc', 'def', 'ghij' ]) => [ 0, 1, 2, 3, 4 ]
- *    getStringsLength([ 'angular', 'react', 'ember' ]) => [ 7, 5, 5 ]
- */
 function getStringsLength(arr) {
   return arr.map((el) => el.length);
 }
 
-/**
- * Returns the average of all items in the specified array of numbers.
- * The result should be rounded to two decimal places.
- *
- * @param {array} arr - The input array
- * @return {number} - The average of all items
- *
- * @example
- *   getAverage([]) => 0
- *   getAverage([ 1, 2, 3 ]) => 2
- *   getAverage([ -1, 1, -1, 1 ]) => 0
- *   getAverage([ 1, 10, 100, 1000 ])  => 277,75
- *   getAverage([ 2, 3, 3 ])  => 2,67
- */
-function getAverage(/* arr */) {
-  throw new Error('Not implemented');
+function getAverage(arr) {
+  return arr.length > 0
+    ? parseFloat((arr.reduce((a, b) => a + b) / arr.length).toFixed(2))
+    : 0;
 }
 
-/**
- * Checks if all strings in an array have the same length.
- *
- * @param {array} arr - The array of strings to be checked.
- * @return {boolean} - True if all strings have the same length, false otherwise.
- *
- * @example
- *    isSameLength(['orange', 'banana', 'cherry']) => true
- *    isSameLength(['cat', 'dog', 'elephant']) => false
- */
-function isSameLength(/* arr */) {
-  throw new Error('Not implemented');
+function isSameLength(arr) {
+  return arr.every((el) => el.length === arr[0].length);
 }
 
-/**
- * Checks if there are elements in the array where the value is equal to its index.
- *
- * @param {array} arr - The array of elements to be checked.
- * @return {boolean} - True if there are elements with value equal to their index, false otherwise.
- *
- * @example
- *    isValueEqualsIndex([0, 1, 2, 3, 4]) => true
- *    isValueEqualsIndex([2, 1, 0, 4, 5]) => true
- *    isValueEqualsIndex([10, 20, 30, 40, 50]) => false
- */
-function isValueEqualsIndex(/* arr */) {
-  throw new Error('Not implemented');
+function isValueEqualsIndex(arr) {
+  return arr.some((el, index) => el === index);
 }
 
-/**
- * Inserts the item into specified array at specified index.
- *
- * @param {array} arr - The input array.
- * @param {any} item - The item to insert.
- * @param {number} index - Specified index.
- *
- * @example
- *    insertItem([ 1, 3, 4, 5 ], 2, 1)  => [ 1, 2, 3, 4, 5 ]
- *    insertItem([ 1, 'b', 'c'], 'x', 0) => [ 'x', 1, 'b', 'c' ]
- */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
-/**
- * Returns the n first items of the specified array.
- *
- * @param {array} arr - The input array.
- * @param {number} n - Number of items.
- *
- * @example
- *    getHead([ 1, 3, 4, 5 ], 2) => [ 1, 3 ]
- *    getHead([ 'a', 'b', 'c', 'd'], 3) => [ 'a', 'b', 'c' ]
- *    getHead([ 'a', 'b', 'c', 'd'], 0) => []
- */
-function getHead(/* arr, n */) {
-  throw new Error('Not implemented');
+function getHead(arr, n) {
+  return arr.slice(0, n);
 }
 
-/**
- * Returns the n last items of the specified array.
- *
- * @param {array} arr - The input array.
- * @param {number} n - Number of items.
- *
- * @example
- *    getTail([ 1, 3, 4, 5 ], 2) => [ 4, 5 ]
- *    getTail([ 'a', 'b', 'c', 'd'], 3) => [ 'b', 'c', 'd' ]
- *    getTail([ 'a', 'b', 'c', 'd'], 0) => []
- */
-function getTail(/* arr, n */) {
-  throw new Error('Not implemented');
+function getTail(arr, n) {
+  return n === 0 ? [] : arr.slice(-n);
 }
 
-/**
- * Returns the doubled array - elements of the specified array
- * are repeated twice using original order.
- *
- * @param {array} arr - The input array.
- * @return {array} - The doubled array.
- *
- * @example
- *    doubleArray(['Ace', 10, true])  => ['Ace', 10, true, 'Ace', 10, true]
- *    doubleArray([0, 1, 2, 3, 4, 5]) => [0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5]
- *    doubleArray([]) => []
- */
-function doubleArray(/* arr */) {
-  throw new Error('Not implemented');
+function doubleArray(arr) {
+  return arr.concat(arr);
 }
 
-/**
- * Concatenates all elements from specified array into single string with ',' delimiter.
- *
- * @param {array} arr - The input array.
- * @return {string} - The concatenated string.
- *
- * @example
- *    toStringList([0, false, 'cat', NaN, true, '']) => '0,false,cat,NaN,true,'
- *    toStringList([1, 2, 3, 4, 5]) => '1,2,3,4,5'
- *    toStringList(['rock', 'paper', 'scissors']) => 'rock,paper,scissors'
- */
-function toStringList(/* arr */) {
-  throw new Error('Not implemented');
+function toStringList(arr) {
+  return arr.join(',');
 }
 
-/**
- * Returns array containing only unique values from the specified array.
- *
- * @param {array} arr - The input array.
- * @return {array} - The array with unique values.
- *
- * @example
- *   distinct([ 1, 2, 3, 3, 2, 1 ]) => [ 1, 2, 3 ]
- *   distinct([ 'a', 'a', 'a', 'a' ])  => [ 'a' ]
- *   distinct([ 1, 1, 2, 2, 3, 3, 4, 4]) => [ 1, 2, 3, 4]
- *   distinct([]) => []
- */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return Array.from(new Set(arr));
 }
 
 /**
@@ -257,158 +75,56 @@ function distinct(/* arr */) {
  *    createNDimensionalArray(4, 2) => [[[[0, 0], [0, 0]], [[0, 0], [0, 0]]], [[[0, 0], [0, 0]], [[0, 0], [0, 0]]]]
  *    createNDimensionalArray(1, 1) => [0]
  */
-function createNDimensionalArray(/* n, size */) {
-  throw new Error('Not implemented');
+function createNDimensionalArray(n, size) {
+  return n === 1
+    ? Array(size).fill(0)
+    : Array(size)
+        .fill(0)
+        .map(() => createNDimensionalArray(n - 1, size));
 }
 
-/**
- * Flattens a nested array into a single-level array.
- *
- * @param {array} nestedArray - The nested array to be flattened.
- * @return {array} - A single-level array.
- *
- * @example
- *    flattenArray([1, [2, [3, 4], 5], 6]) => [1, 2, 3, 4, 5, 6]
- *    flattenArray(['a', ['b', ['c', 'd'], 'e'], 'f']) => ['a', 'b', 'c', 'd', 'e', 'f']
- *    flattenArray([1, 2, 3, 4]) => [1, 2, 3, 4]
- */
-function flattenArray(/* nestedArray */) {
-  throw new Error('Not implemented');
+function flattenArray(nestedArray) {
+  return nestedArray.flat(Infinity);
 }
 
-/**
- * Projects each element of the specified array to a sequence
- * and flattens the resulting sequences into one array.
- *
- * @param {array} arr - The input array
- * @param {Function} childrenSelector - A transform function to apply to each element
- *                                     that returns an array of children
- * @return {array} - The flatted array
- *
- * @example
- *   selectMany([[1, 2], [3, 4], [5, 6]], (x) => x) =>   [ 1, 2, 3, 4, 5, 6 ]
- *   selectMany(['one','two','three'], (x) => x.split('')) =>   ['o','n','e','t','w','o','t','h','r','e','e']
- */
-function selectMany(/* arr, childrenSelector */) {
-  throw new Error('Not implemented');
+function selectMany(arr, childrenSelector) {
+  return arr.flatMap(childrenSelector);
 }
 
-/**
- * Every month, you record your income and expenses.
- * Expenses may be greater than income.
- * You need to calculate the final balance.
- *
- * @param {array} arr - The input array [[income, expence], ...]
- * @return {number} - The final balance.
- *
- * @example
- *   calculateBalance([ [ 10, 8 ], [ 5, 1 ] ]) => (10 - 8) + (5 - 1) = 2 + 4 = 6
- *   calculateBalance([ [ 10, 8 ], [ 1, 5 ] ])  => (10 - 8) + (1 - 5) = 2 + -4 = -2
- *   calculateBalance([]) => 0
- */
-function calculateBalance(/* arr */) {
-  throw new Error('Not implemented');
+function calculateBalance(arr) {
+  return arr.reduce((acc, curr) => acc + curr[0] - curr[1], 0);
 }
 
-/**
- * Breaks an array into chunks of the specified size.
- *
- * @param {array} arr - The array to be broken into chunks.
- * @param {number} chunkSize - The size of each chunk.
- * @return {array} - An array of chunks.
- *
- * @example
- *    createChunks([1, 2, 3, 4, 5, 6, 7], 3) => [[1, 2, 3], [4, 5, 6], [7]]
- *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
- *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
- */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  return Array(Math.ceil(arr.length / chunkSize))
+    .fill(null)
+    .map(() => arr.splice(0, chunkSize));
 }
 
-/**
- * Generates an array of odd numbers of the specified length.
- *
- * @param {number} len - The length of an array.
- * @return {array} - An array of odd numbers.
- *
- * @example
- *    generateOdds(0) => []
- *    generateOdds(1) => [ 1 ]
- *    generateOdds(2) => [ 1, 3 ]
- *    generateOdds(5) => [ 1, 3, 5, 7, 9 ]
- */
-function generateOdds(/* len */) {
-  throw new Error('Not implemented');
+function generateOdds(len) {
+  return Array(len)
+    .fill(1)
+    .map((el, index) => el + index * 2);
 }
 
-/**
- * Returns an element from the multidimensional array by the specified indices.
- *
- * @param {array} arr - The input multidimensional array
- * @param {array} indices - The array of indices
- * @return {any} - An element from the array
- *
- * @example
- *   getElementByIndices([[1, 2], [3, 4], [5, 6]], [0,0]) => 1        (arr[0][0])
- *   getElementByIndices(['one','two','three'], [2]) => 'three'  (arr[2])
- *   getElementByIndices([[[ 1, 2, 3]]], [ 0, 0, 1 ]) => 2        (arr[0][0][1])
- */
-function getElementByIndices(/* arr, indices */) {
-  throw new Error('Not implemented');
+function getElementByIndices(arr, indices) {
+  return indices.reduce((res, index) => res[index], arr);
 }
 
-/**
- * Returns the number of all falsy values in the specified array.
- *
- * @param {array} arr - The input array.
- * @return {number} - The number of all falsy values.
- *
- * @example
- *  getFalsyValuesCount([]) => 0
- *  getFalsyValuesCount([ 1, '', 3 ]) => 1
- *  getFalsyValuesCount([ -1, 'false', null, 0 ]) => 2
- *  getFalsyValuesCount([ null, undefined, NaN, false, 0, '' ]) => 6
- */
-function getFalsyValuesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getFalsyValuesCount(arr) {
+  return arr.filter((el) => !el).length;
 }
 
-/**
- * Creates an identity matrix of the specified size.
- *
- * @param {number} n - A size of the matrix.
- * @return {array} - An identity matrix.
- *
- * @example
- *     getIdentityMatrix(1)  => [[1]]
- *
- *     getIdentityMatrix(2) => [[1,0],
- *                             [0,1]]
- *
- *                              [[1,0,0,0,0],
- *                              [0,1,0,0,0],
- *     getIdentityMatrix(5) =>  [0,0,1,0,0],
- *                              [0,0,0,1,0],
- *                              [0,0,0,0,1]]
- */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from({ length: n }, (_, i) =>
+    Array.from({ length: n }, (__, j) => (i === j ? 1 : 0))
+  );
 }
 
-/**
- * Returns an array containing indices of odd elements in the input array.
- *
- * @param {array} numbers - The array of numbers.
- * @return {array} - An array containing indices of odd elements.
- *
- * @example
- *    getIndicesOfOddNumbers([1, 2, 3, 4, 5]) => [0, 2, 4]
- *    getIndicesOfOddNumbers([2, 4, 6, 8, 10]) => []
- *    getIndicesOfOddNumbers([11, 22, 33, 44, 55]) => [0, 2, 4]
- */
-function getIndicesOfOddNumbers(/* numbers */) {
-  throw new Error('Not implemented');
+function getIndicesOfOddNumbers(numbers) {
+  return numbers
+    .filter((val) => val % 2 !== 0)
+    .map((val) => numbers.indexOf(val));
 }
 
 /**
@@ -421,8 +137,10 @@ function getIndicesOfOddNumbers(/* numbers */) {
  *    getHexRGBValues([ 0, 255, 16777215]) => [ '#000000', '#0000FF', '#FFFFFF' ]
  *    getHexRGBValues([]) => []
  */
-function getHexRGBValues(/* arr */) {
-  throw new Error('Not implemented');
+function getHexRGBValues(arr) {
+  return arr.map(
+    (val) => `#${val.toString(16).padStart(6, '0').toUpperCase()}`
+  );
 }
 
 /**
